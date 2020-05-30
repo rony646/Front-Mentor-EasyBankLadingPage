a0a8e675a29d198837b2ba2fdfbf2a4e60bfe8b1
rony646 <rony646@users.noreply.github.com>
2020-05-29 22:57:36 -0300

    html fixes

---
+++ scripts/sideBar.js
@@ -0,0 +1,18 @@
+const openButton = document.querySelector('#open');
+const menu = document.querySelector('.menu')
+
+openButton.addEventListener('click', () => {
+    if(openButton.classList.contains('closed')) {
+        openButton.src = '../images/icon-close.svg';
+        openButton.classList.remove('closed');
+        openButton.classList.add('opened');
+        menu.style.display = 'flex'
+    }
+
+    else if(openButton.classList.contains('opened')) {
+        openButton.src = './../images/icon-hamburger.svg';
+        openButton.classList.remove('opened');
+        openButton.classList.add('closed');
+        menu.style.display = 'none';
+    }
+})
